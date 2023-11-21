dd0c7fa52f7e7e919648a4f889078d69495fbae6
sebaek <sebaek@gmail.com>
2023-11-21 14:38:41 +0900

    첨부파일 있는 게시물 수정 기능 작성중....

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -2,7 +2,9 @@ import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
+  Image,
   Input,
   Modal,
   ModalBody,
@@ -12,18 +14,23 @@ import {
   ModalHeader,
   ModalOverlay,
   Spinner,
+  Switch,
   Textarea,
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
 import { logDOM } from "@testing-library/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
+  const [removeFileIds, setRemoveFileIds] = useState([]);
+  const [uploadFiles, setUploadFiles] = useState(null);
 
   // /edit/:id
   const { id } = useParams();
@@ -47,7 +54,13 @@ export function BoardEdit() {
     // PUT /api/board/edit
 
     axios
-      .put("/api/board/edit", board)
+      .putForm("/api/board/edit", {
+        id: board.id,
+        title: board.title,
+        content: board.content,
+        removeFileIds,
+        uploadFiles,
+      })
       .then(() => {
         toast({
           description: board.id + "번 게시글이 수정되었습니다.",
@@ -70,6 +83,16 @@ export function BoardEdit() {
         }
       })
       .finally(() => onClose());
+  }
+
+  function handleRemoveFileSwitch(e) {
+    if (e.target.checked) {
+      // removeFileIds 에 추가
+      setRemoveFileIds([...removeFileIds, e.target.value]);
+    } else {
+      // removeFileIds 에서 삭제
+      setRemoveFileIds(removeFileIds.filter((item) => item !== e.target.value));
+    }
   }
 
   return (
@@ -97,7 +120,39 @@ export function BoardEdit() {
           }
         />
       </FormControl>
+      {/* 이미지 출력 */}
+      {board.files.length > 0 &&
+        board.files.map((file) => (
+          <Box key={file.id} my="5px" border="3px solid black">
+            <FormControl display="flex" alignItems="center">
+              <FormLabel>
+                <FontAwesomeIcon color="red" icon={faTrashCan} />
+              </FormLabel>
+              <Switch
+                value={file.id}
+                colorScheme="red"
+                onChange={handleRemoveFileSwitch}
+              />
+            </FormControl>
+            <Box>
+              <Image src={file.url} alt={file.name} width="100%" />
+            </Box>
+          </Box>
+        ))}
 
+      {/* 추가할 파일 선택 */}
+      <FormControl>
+        <FormLabel>이미지</FormLabel>
+        <Input
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={(e) => setUploadFiles(e.target.files)}
+        />
+        <FormHelperText>
+          한 개 파일은 1MB 이내, 총 용량은 10MB 이내로 첨부하세요.
+        </FormHelperText>
+      </FormControl>
       <Button colorScheme="blue" onClick={onOpen}>
         저장
       </Button>
